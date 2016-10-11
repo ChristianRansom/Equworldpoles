00cfe92d1a145b3fc950bd8b5b26c1f9fdd2cdf5
Christian <christian.ransom123@gmail.com>
2016-10-11 13:44:37 -0700

    Begin setting up class structure.

--- src/main/java/ransom/minecraft/plugin/development/FenceBar.java
+++ src/main/java/ransom/minecraft/plugin/development/FenceBar.java
@@ -1,5 +1,49 @@
 package ransom.minecraft.plugin.development;
 
+import java.util.ArrayList;
+import org.bukkit.Location;
+
 public class FenceBar {
 
+	private ArrayList<FenceTop> fenceTops = new ArrayList<FenceTop>();
+	private Location resetSign = new Location(null, 0, 0, 0);
+	boolean set;
+	
+	public FenceBar(){
+		this.set = true; 
+	}
+	
+	public void addFenceTop(){
+		
+	}
+	
+	//Called on Player Move
+	public boolean isHit(Location playerLoc){
+		/*whenPlayerMove()
+	     when riding horse
+	          for each fencebar
+	               if fencebar.isHit()
+	                    fencebar.fall()
+	                    this.set = false*/
+						fall();
+		
+		
+		return false;
+	}
+	
+	
+	/*Method abstracted from isHit() for better code blocking 
+	and clear logic flow*/
+	private void fall(){
+		
+	}
+	
+	public boolean set(){
+		
+		
+		return false;
+	}
+	
+	
+	
 }
--- src/main/java/ransom/minecraft/plugin/development/FenceTop.java
+++ src/main/java/ransom/minecraft/plugin/development/FenceTop.java
@@ -1,5 +1,20 @@
 package ransom.minecraft.plugin.development;
 
-public class FenceTop {
+import org.bukkit.Location;
 
+public class FenceTop {
+	private double minX;
+	private double maxX;
+	private double minY;
+	private double maxY;
+	
+	public FenceTop(double xMin, double xMax, double yMin, double yMax){
+		
+		
+	}
+	
+	public FenceTop(Location playerLoc){
+		
+	}
+	
 }
